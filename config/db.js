f170b754d762ae6c33c09cd4198fcbb9376ea427
Sanskar00 <saxenasanskar090901@gmail.com>
2021-10-08 10:28:14 +0530

    Neq routes folder created containing auth posts profile users

--- config/db.js
+++ config/db.js
@@ -1,10 +1,12 @@
 const mongoose = require("mongoose");
-const config = require("require");
+const config = require("config");
 const db = config.get("mongoURI");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(db);
+    await mongoose.connect(db, {
+      useNewUrlParser: true,
+    });
     console.log("MongoDB Connected...");
   } catch (err) {
     console.log(err.message);
